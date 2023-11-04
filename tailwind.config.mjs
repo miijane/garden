ee58f8e28010c26511e09680a88321842e5bc668
miijane <mijane.404@gmail.com>
2023-11-05 07:39:38 +0800

    🚧 [add] create navbar

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,16 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'rust-grey': '#47585C',
+				'willow-grey': '#C8D5BB'
+			},
+			fontFamily: {
+				'epiloge': ['Epilogue', 'sans-serif']
+			}
+		},
 	},
 	plugins: [],
+	darkMode: 'class',
 }
